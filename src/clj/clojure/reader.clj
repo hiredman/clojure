fa8deac15042bb7e665d619645bd91118907aeac
Kevin Downey <redchin@gmail.com>
2009-10-20 21:18:23 -0700

    readunicodechar

--- src/clj/clojure/reader.clj
+++ src/clj/clojure/reader.clj
@@ -5,13 +5,45 @@
     (doto (.setAccessible true))
     (.invoke obj (into-array Object args))))
 
+(defn- terminating-macro? [ch]
+  (clojure.lang.LispReader/isTerminatingMacro (int ch)))
+
+(defn- macro? [ch]
+  (clojure.lang.LispReader/isMacro (int ch)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defn- readunicodechar [& foo]
   (apply wall-hack-method clojure.lang.LispReader
          :readUnicodeChar
          [java.io.PushbackReader int int int boolean] nil
          foo))
 
-(defn STRINGREADER [#^java.io.Reader reader doublequote]
+(defn- whitespace? [char-or-int]
+  (or (Character/isWhitespace (char char-or-int)) (= char-or-int \,)))
+
+(defmulti #^{:private true} readunicodechar (comp type first list))
+
+(defmethod readunicodechar java.io.PushbackReader [r initch base length exact]
+  (let [uc (Character/digit initch base)]
+    (when (= -1 uc)
+      (throw (IllegalArgumentException. (str "Invalid digit: " initch))))
+    (loop [i 1 uc uc]
+      (if (< i length)
+        (let [ch (.read r)]
+          (if (or (= ch -1)
+                  (whitespace? ch)
+                  (macro? ch))
+            (.unread r ch)
+            (let [d (Character/digit ch base)]
+              (when (= d -1)
+                (throw (IllegalArgumentException. (str "Invalid digit: " (char ch)))))
+              (recur (inc i) (* uc (+ base d))))))
+        (if (and exact
+                 (not= i length))
+          (throw (IllegalArgumentException. (str "Invalid character length: " i ", shoulde be: " length)))
+          uc)))))
+
+(defn- STRINGREADER [#^java.io.Reader reader doublequote]
   (let [sb (StringBuilder.)]
     (loop []
       (let [ch (char (.read reader))]
@@ -39,7 +71,7 @@
                        ch))
             (recur)))))))
 
-(defn COMMENTREADER [reader semicolon]
+(defn- COMMENTREADER [reader semicolon]
   (loop [ch (char (.read reader))]
     (if (and (not= -1 (int ch))
              (not= \newline ch)
@@ -47,8 +79,25 @@
       (recur (char (.read reader)))
       reader)))
 
-(defn QUOTEWRAPPINGREADER [reader quo]
-  (clojure.lang.RT/list 'quote (read reader true nil true)))
+(defn- wrapping-reader [sym]
+  (fn [reader quo]
+    (clojure.lang.RT/list sym (read reader true nil true))))
 
-(defn DEREFWRAPPINGREADER [reader quo]
-  (clojure.lang.RT/list 'deref (read reader true nil true)))
+(def #^{:private true} QUOTEWRAPPINGREADER (wrapping-reader 'quote))
+(def #^{:private true} DEREFWRAPPINGREADER (wrapping-reader 'deref))
+(def #^{:private true} METAWRAPPINGREADER (wrapping-reader 'meta))
+
+;;(defn- READTOKEN [reader initch]
+;;  (let [sb (StringBuilder.)]
+;;    (.append sb initch)
+;;    (loop [ch (int (.read reader))]
+;;      (if (or (= -1 ch)
+;;              (whitespace? ch)
+;;              (terminating-macro? ch))
+;;        (do (.unread reader ch)
+;;            (.toString sb))
+;;        (do (.append sb (char ch))
+;;            (recur (int (.read reader))))))))
+
+(.println System/err "Reader Online")
+

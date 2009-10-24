298aa758192416047f490e530d26f518f8f81c35
Kevin Downey <redchin@gmail.com>
2009-10-23 23:27:23 -0700

    pushing bits

--- src/clj/clojure/reader.clj
+++ src/clj/clojure/reader.clj
@@ -2,6 +2,11 @@
 ;;; TODO
 ;;; Move ReaderException somewhere so LispReader can be safely deleted
 ;;; ?
+
+(defmacro eof-guard [ch e]
+  `(when (= -1 (int ~ch))
+     (throw ~e)))
+
 (defn- readI [rdr eof-is-error? eof-value recursive?]
   (let [regex (fn [s] (java.util.regex.Pattern/compile s))
         intPat (regex "([-+]?)(?:(0)|([1-9][0-9]*)|0[xX]([0-9A-Fa-f]+)|0([0-7]+)|([1-9][0-9]?)[rR]([0-9A-Za-z]+)|0[0-9]+)")
@@ -35,25 +40,15 @@
             (numbers-divide [n d]
               (clojure.lang.Numbers/divide n d)),
             (array-list [] (java.util.ArrayList.)),
-            (get-macro [ch]
-              (condp = (char ch)
-                \" string-reader
-                \; coment-reader
-                \' (wrapping-reader 'clojure.core/quote)
-                \@ (wrapping-reader 'clojure.core/deref)
-                \^ (wrapping-reader 'clojure.core/meta)
-                \` (clojure.lang.LispReader$SyntaxQuoteReader.)
-                \~ unquote-reader
-                \( list-reader
-                \) unmatched-delimited-reader
-                \[ vector-reader
-                \] unmatched-delimited-reader
-                \{ (clojure.lang.LispReader$MapReader.)
-                \} unmatched-delimited-reader
-                \\ (clojure.lang.LispReader$CharacterReader.)
-                \% arg-reader
-                \# dispatch-reader
-                nil))
+            (namespace-for [kw]
+              (clojure.lang.Compiler/namespaceFor #^clojure.lang.Symbol kw))
+            (current-ns [] (deref clojure.lang.RT/CURRENT_NS))
+            (keyword-intern<2> [ns kw] (clojure.lang.Keyword/intern ns kw))
+            (keyword-intern<1> [kw] (clojure.lang.Keyword/intern kw))
+            (symbol-intern [s] (clojure.lang.Symbol/intern s))
+            (symbol [s] (clojure.lang.Symbol/create s))
+            (syntax-quote-reader []
+              (clojure.lang.LispReader$SyntaxQuoteReader.))
             (get-dispatch-macro [ch]
               (condp = (char ch)
                 \^ (clojure.lang.LispReader$MetaReader.)
@@ -68,18 +63,98 @@
                 nil))
             (read-delimited-list [delim rdr recur?]
               (clojure.lang.LispReader/readDelimitedList delim rdr recur?)),
-            (macth-symbol [s]
-              (clojure.lang.LispReader/matchSymbol s)),
-            (read-uncode-char [rdr ch base length exact]
-              (clojure.lang.LispReader/readUnicodeChar rdr ch base length exact)),
             (register-arg [n]
               (clojure.lang.LispReader/registerArg n))
             (reader-exception [ln msg]
               (clojure.lang.LispReader$ReaderException. ln msg))
             ;/Wrappers;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+            (get-macro [ch]
+              (condp = (char ch)
+                \" string-reader
+                \; coment-reader
+                \' (wrapping-reader 'clojure.core/quote)
+                \@ (wrapping-reader 'clojure.core/deref)
+                \^ (wrapping-reader 'clojure.core/meta)
+                \` (syntax-quote-reader) 
+                \~ unquote-reader
+                \( list-reader
+                \) unmatched-delimited-reader
+                \[ vector-reader
+                \] unmatched-delimited-reader
+                \{ map-reader
+                \} unmatched-delimited-reader
+                \\ character-reader
+                \% arg-reader
+                \# dispatch-reader
+                nil))
+            (character-reader [rdr backslash]
+              (let [ch (.read rdr)]
+                (eof-guard ch (Exception. "EOF while reading character"))
+                (let [token (read-token rdr ch)]
+                  (if (= 1 (.length token))
+                    (Character/valueOf (.charAt token 0))
+                    (condp = token
+                      "newline" \n
+                      "space" \space
+                      "tab" \t
+                      "backspace" \b
+                      "formfeed" \f
+                      "return" \r
+                      (cond
+                        (.startsWith token "u")
+                          (let [c (read-unicode-char<string> token 1 4 16)]
+                            (if (and (or (= (int c) (+ 1 (int \uD799)))
+                                         (> (int c) (+ 1 (int \uD799))))
+                                     (or (= (int c) (- (int \uF000) 1))
+                                         (< (int c) (- (int \uF000) 1))))
+                              (throw (Exception. (format "Invalid character constant \\u%s" (Integer/toString (int c) 16))))
+                              c))
+                        (.startsWith token "o")
+                          (let [len (- (.length token) 1)]
+                            (if (> len 3)
+                              (throw (Exception. (format "Invalid octal escape sequence length: %i" len)))
+                              (let [uc (read-unicode-char<string> token 1 len 8)]
+                                (if (> uc 0377)
+                                  (throw (Exception. "Octal escape sequence must be in range [0, 377]."))
+                                  uc))))
+                        :else
+                          (throw (Exception. (format "Unsupported character: \\%s" token)))))))))
+            (read-unicode-char<string> [token offset length base]
+              (if (not (= (.length token) (+ offset length)))
+                (throw (IllegalArgumentException. (format "Invalid unicode character: \\%s" token)))
+                (loop [i offset uc 0]
+                  (if (> offset i)
+                    (let [d (Character/digit (.charAt token i) base)]
+                      (eof-guard d (IllegalArgumentException. (format "Invalid digit: %c" (char d))))
+                      (recur (inc i) (+ d (* uc base))))
+                    uc))))
+            (read-unicode-char<pushback> [rdr ch base length exact]
+              (letfn [(break [uc i length exact]
+                        (if (and exact (not (= i length)))
+                          (throw
+                            (IllegalArgumentException.
+                              (format "Invalid character length: %i should be %i" i length)))
+                          uc))]
+                (let [uc (Character/digit ch base)]
+                  (eof-guard uc (IllegalArgumentException. (format "Invalid digit: %c" (char ch))))
+                  (loop [i 1 uc uc]
+                    (if (< i length)
+                      (break uc i length exact)
+                      (let [ch (.read rdr)]
+                        (if (or (= -1 (int ch))
+                                (whitespace? ch)
+                                (macro? ch))
+                          (do (.unread rdr ch)
+                            (break uc i length exact))
+                          (let [d (Character/digit ch base)]
+                            (if (= -1 d)
+                              (throw
+                                (IllegalArgumentException.
+                                  (format "Invalid digit: %c" (char ch))))
+                              (recur (inc i) (+ d (* uc base)))))))))))),
             (match-symbol [s]
               (let [m (.matcher symbolPat s)]
-                (if (.matches m)
+                (when (.matches m)
                   (let [gc (.groupCount m)
                         ns (.group m 1)
                         name (.group 2)]
@@ -88,7 +163,16 @@
                             (.endsWith ns ":")
                             (not (= -1 (.indexOf ns "::" 1))))
                       nil
-                      (if (.startsWith s "::")))))))
+                      (if (.startsWith s "::")
+                        (let [ks (symbol (.substring s 2))
+                              kns (if (nil? (.ns ks))
+                                    (namespace-for ks)
+                                    (current-ns))]
+                          (keyword-intern<2> (.name (.name kns)) (.name ks)))
+                        (let [keyword? (= (.charAt s 0) \:)
+                              sym (symbol-intern (.substring s (if keyword? 1 0)))]
+                          (when keyword?
+                            (keyword-intern<1> sym))))))))),
             (match-number [s]
               (let [m (.matcher intPat s)]
                 (if (.matches m)
@@ -117,14 +201,14 @@
                         (when (.matches m)
                           (numbers-divide
                             (BigInteger. (.group m 1))
-                            (BigInteger. (.group m 2))))))))))
+                            (BigInteger. (.group m 2)))))))))),
             (var-reader [rdr quo]
-              (list 'var (read rdr true nil true)))
+              (list 'var (read rdr true nil true))),
             (unmatched-delimited-reader [rdr rightdelim]
               (throw
                 (Exception.
-                  (format "Unmatched delimiter: %c" (char rightdelim)))))
-            (whitespace? [ch] (or (Character/isWhitespace ch) (= \, (char ch))))
+                  (format "Unmatched delimiter: %c" (char rightdelim))))),
+            (whitespace? [ch] (or (Character/isWhitespace ch) (= \, (char ch)))),
             (arg-reader [rdr pct]
               (if (nil? (arg-env))
                 (interpret-token (read-token rdr \%))
@@ -196,9 +280,9 @@
                                   \u (let [ch (.read rdr)]
                                        (if (= -1 (Character/digit (char ch) 16))
                                          (throw (Exception. (str "Invalid unicode escape: \\u" (char ch))))
-                                         (char (read-uncode-char rdr ch 16 4 true))))
+                                         (char (read-unicode-char<pushback> rdr ch 16 4 true))))
                                   (if (Character/isDigit ch)
-                                    (let [ch (read-uncode-char rdr ch 8 3 false)]
+                                    (let [ch (read-unicode-char<pushback> rdr ch 8 3 false)]
                                       (if (> ch 0377)
                                         (throw (Exception. "Octal escape sequence must be in range [0, 377]."))
                                         (char ch)))
@@ -217,7 +301,7 @@
                 "false" false
                 "/" (symbol "/")
                 "clojure.core//" (symbol "clojure.core" "/")
-                (let [ret (macth-symbol token)]
+                (let [ret (match-symbol token)]
                   (if (not (nil? ret))
                     ret
                     (throw (Exception. (str "Invalid token: " + token)))))))
@@ -247,6 +331,17 @@
                           n)))
                     (do (.append sb (char ch))
                       (recur (.read rdr)))))))
+            (map-reader [rdr leftparen]
+              (let [line (if (instance? clojure.lang.LineNumberingPushbackReader rdr)
+                           (.getLineNumber rdr)
+                           -1)
+                    list (read-delimited-list \} rdr true)]
+                (if (.isEmpty list)
+                  ()
+                  (let [s (clojure.lang.RT/map (.toArray list))]
+                    (if (not (= -1 line))
+                      (.withMeta s {:line line})
+                      s)))))
             (vector-reader [rdr leftparen]
               (let [line (if (instance? clojure.lang.LineNumberingPushbackReader rdr)
                            (.getLineNumber rdr)
@@ -322,8 +417,8 @@
     (binding [*out* file]
       (.println *out* (str "reader.name=" (.getName (class readI)))))))
 
-(defmacro lett [bindings & body]
-  (let [[name value & xs] bindings]
-    (if (not (nil? name))
-      `((fn [~name] (lett ~(vec xs) ~@body)) ~value)
-      `(do ~@body))))
+;;(defmacro lett [bindings & body]
+;;  (let [[name value & xs] bindings]
+;;    (if (not (nil? name))
+;;      `((fn [~name] (lett ~(vec xs) ~@body)) ~value)
+;;      `(do ~@body))))

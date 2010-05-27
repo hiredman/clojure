7cc3a9e748d9027281e805a24bf1a27c88ecb49f
Kevin <kevin.downey@sonian.net>
2010-05-27 15:53:44 -0700

    use weakrefs for keywords

--- src/jvm/clojure/lang/Keyword.java
+++ src/jvm/clojure/lang/Keyword.java
@@ -4,7 +4,7 @@
  *   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
  *   which can be found in the file epl-v10.html at the root of this distribution.
  *   By using this software in any fashion, you are agreeing to be bound by
- * 	 the terms of this license.
+ *       the terms of this license.
  *   You must not remove this notice, or any other, from this software.
  **/
 
@@ -14,202 +14,214 @@ package clojure.lang;
 
 import java.io.ObjectStreamException;
 import java.io.Serializable;
-import java.util.concurrent.ConcurrentHashMap;
-
+import java.lang.ref.WeakReference;
+import java.util.WeakHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantLock;
 
 public final class Keyword implements IFn, Comparable, Named, Serializable {
 
-private static ConcurrentHashMap<Symbol, Keyword> table = new ConcurrentHashMap();
-public final Symbol sym;
-final int hash;
+    private static Map<Symbol, WeakReference<Keyword>> table =
+        new WeakHashMap();
+    private static ReentrantLock lock = new ReentrantLock();
+    public final Symbol sym;
+    final int hash;
 
-public static Keyword intern(Symbol sym){
-	Keyword k = new Keyword(sym);
-	Keyword existingk = table.putIfAbsent(sym, k);
-	return existingk == null ? k : existingk;
-}
+    public static Keyword intern(Symbol sym){
+        try {
+            lock.lock();
+            WeakReference<Keyword> wexistingk = table.get(sym);
+            if (wexistingk == null || wexistingk.get() == null) {
+                wexistingk = new WeakReference(new Keyword(sym));
+                table.put(sym,wexistingk);
+            }
+            return wexistingk.get();
+        } finally {
+            lock.unlock();
+        }
+    }
 
-public static Keyword intern(String ns, String name){
-	return intern(Symbol.intern(ns, name));
-}
+    public static Keyword intern(String ns, String name){
+        return intern(Symbol.intern(ns, name));
+    }
 
-public static Keyword intern(String nsname){
-	return intern(Symbol.intern(nsname));
-}
+    public static Keyword intern(String nsname){
+        return intern(Symbol.intern(nsname));
+    }
 
-private Keyword(Symbol sym){
-	this.sym = sym;
-	hash = sym.hashCode() + 0x9e3779b9;
-}
+    private Keyword(Symbol sym){
+        this.sym = sym;
+        hash = sym.hashCode() + 0x9e3779b9;
+    }
 
-public final int hashCode(){
-	return hash;
-}
+    public final int hashCode(){
+        return hash;
+    }
 
-public String toString(){
-	return ":" + sym;
-}
+    public String toString(){
+        return ":" + sym;
+    }
 
-public Object throwArity(){
-	throw new IllegalArgumentException("Wrong number of args passed to keyword: "
-	                                   + toString());
-}
+    public Object throwArity(){
+        throw new IllegalArgumentException("Wrong number of args passed to keyword: "
+                                           + toString());
+    }
 
-public Object call() throws Exception{
-	return throwArity();
-}
+    public Object call() throws Exception{
+        return throwArity();
+    }
 
-public void run(){
-	throw new UnsupportedOperationException();
-}
+    public void run(){
+        throw new UnsupportedOperationException();
+    }
 
-public Object invoke() throws Exception{
-	return throwArity();
-}
+    public Object invoke() throws Exception{
+        return throwArity();
+    }
 
-public int compareTo(Object o){
-	return sym.compareTo(((Keyword) o).sym);
-}
-
-
-public String getNamespace(){
-	return sym.getNamespace();
-}
-
-public String getName(){
-	return sym.getName();
-}
-
-private Object readResolve() throws ObjectStreamException{
-	return intern(sym);
-}
-
-/**
- * Indexer implements IFn for attr access
- *
- * @param obj - must be IPersistentMap
- * @return the value at the key or nil if not found
- * @throws Exception
- */
-final public Object invoke(Object obj) throws Exception{
-	if(obj instanceof ILookup)
-		return ((ILookup)obj).valAt(this);
-	return RT.get(obj, this);
-}
-
-final public Object invoke(Object obj, Object notFound) throws Exception{
-	if(obj instanceof ILookup)
-		return ((ILookup)obj).valAt(this,notFound);
-	return RT.get(obj, this, notFound);
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7)
-		throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13)
-		throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14)
-		throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
-                     Object arg15) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
-                     Object arg15, Object arg16) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
-                     Object arg15, Object arg16, Object arg17) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
-                     Object arg15, Object arg16, Object arg17, Object arg18) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
-                     Object arg15, Object arg16, Object arg17, Object arg18, Object arg19) throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
-                     Object arg15, Object arg16, Object arg17, Object arg18, Object arg19, Object arg20)
-		throws Exception{
-	return throwArity();
-}
-
-public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
-                     Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
-                     Object arg15, Object arg16, Object arg17, Object arg18, Object arg19, Object arg20,
-                     Object... args)
-		throws Exception{
-	return throwArity();
-}
+    public int compareTo(Object o){
+        return sym.compareTo(((Keyword) o).sym);
+    }
 
 
-public Object applyTo(ISeq arglist) throws Exception{
-	return AFn.applyToHelper(this, arglist);
-}
+    public String getNamespace(){
+        return sym.getNamespace();
+    }
+
+    public String getName(){
+        return sym.getName();
+    }
+
+    private Object readResolve() throws ObjectStreamException{
+        return intern(sym);
+    }
+
+    /**
+     * Indexer implements IFn for attr access
+     *
+     * @param obj - must be IPersistentMap
+     * @return the value at the key or nil if not found
+     * @throws Exception
+     */
+    final public Object invoke(Object obj) throws Exception{
+        if(obj instanceof ILookup)
+            return ((ILookup)obj).valAt(this);
+        return RT.get(obj, this);
+    }
+
+    final public Object invoke(Object obj, Object notFound) throws Exception{
+        if(obj instanceof ILookup)
+            return ((ILookup)obj).valAt(this,notFound);
+        return RT.get(obj, this, notFound);
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7)
+        throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13)
+        throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14)
+        throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
+                         Object arg15) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
+                         Object arg15, Object arg16) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
+                         Object arg15, Object arg16, Object arg17) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
+                         Object arg15, Object arg16, Object arg17, Object arg18) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
+                         Object arg15, Object arg16, Object arg17, Object arg18, Object arg19) throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
+                         Object arg15, Object arg16, Object arg17, Object arg18, Object arg19, Object arg20)
+        throws Exception{
+        return throwArity();
+    }
+
+    public Object invoke(Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6, Object arg7,
+                         Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13, Object arg14,
+                         Object arg15, Object arg16, Object arg17, Object arg18, Object arg19, Object arg20,
+                         Object... args)
+        throws Exception{
+        return throwArity();
+    }
+
+
+    public Object applyTo(ISeq arglist) throws Exception{
+        return AFn.applyToHelper(this, arglist);
+    }
 
 
 }
